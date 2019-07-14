bd7585e4de8283b4462e851581fc5c1b4312dd9a
Mohamed Gamal <mohamed.gamal987.mg@gmail.com>
2019-07-14 19:24:15 +0200

    init newspaper with admin lte

---
+++ src/main/java/com/domain/controller/Test.java
@@ -0,0 +1,12 @@
+package com.domain.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Test {
+	@RequestMapping("/")
+	private String test() {
+		return "index";
+	}
+}
